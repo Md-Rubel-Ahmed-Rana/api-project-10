b6258e05bbff1334b754e8a6862dde03c99d9c26
Md Rubel Ahmed Rana <mdrubelahmedrana521@gmail.com>
2022-09-02 01:33:46 +0600

    Product displayed to the UI successfully

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ const loadData = async() => {
 
     return data;
 };
+
+// product container
+const productContainer = document.getElementById("product-container")
+
+// show the products to the UI
+const showProducts = async() => {
+    const products = await loadData();
+    products.forEach(product => {
+        const productDiv = document.createElement("div");
+        productDiv.classList.add("col");
+        productDiv.innerHTML = `
+            <div class="card p-3">
+                <img src="${product.image}" class="card-img-top" alt="..." style="height: 200px">
+                <div class="card-body">
+                    <h5 class="card-title">${product.title.slice(0,25)}</h5>
+                    <p class="card-text">${product.description.slice(0, 100)}</p>
+                </div>
+            </div>
+        `;
+        productContainer.appendChild(productDiv);
+    });
+};
+
+showProducts()
